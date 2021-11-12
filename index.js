eefbde20ab6e7912ff761fbac0d44a2c07ee841a
Franny Medico <m.francisca.medico@gmail.com>
2021-11-12 12:16:28 +0100

    modidief index to use console.lgo instead of debug

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const chalk = require("chalk");
 
 const debug = require("debug")("npm:index");
 
-debug(chalk.magentaBright("ESTOY FUNCIONANDO"));
+console.log(chalk.magentaBright("ESTOY FUNCIONANDO"));
